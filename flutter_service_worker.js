5b6ddd1a77230ec3bd59b47ce20517d70405778f
matibat <matibat@gmail.com>
2022-05-25 20:42:56 +0000

    Deploying to gh-pages from @ ardriveapp/ardrive-web@dfdb4e263f081d4777ceec44d2ba56cd3d035911 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "220c09335e5d5d88618ddbc24dc40bea",
+  "main.dart.js": "b5cb42ac50d639cb2f982bccd8d62c76",
 "sql-wasm.js": "88a2d15fe24a12bed48ade5c89689065",
 "canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
