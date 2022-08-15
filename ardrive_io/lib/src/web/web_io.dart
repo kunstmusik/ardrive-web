f602b105914ca635441e6f7fba980c09b33891a9
thiagocarvalhodev <thiagocarvalho.dev@gmail.com>
2022-08-15 09:18:12 -0300

    feat(file source) add web implementation

--- ardrive_io/lib/src/web/web_io.dart
+++ ardrive_io/lib/src/web/web_io.dart
@@ -25,7 +25,13 @@ class WebIO implements ArDriveIO {
   final FolderPicker _folderPicker;
 
   @override
-  Future<IOFile> pickFile({List<String>? allowedExtensions}) async {
+  Future<IOFile> pickFile(
+      {List<String>? allowedExtensions,
+      FileSource fileSource = FileSource.fileSystem}) async {
+    if (fileSource != FileSource.fileSystem) {
+      throw UnsupportedPlatformException();
+    }
+
     final file = await file_selector.openFile(acceptedTypeGroups: [
       file_selector.XTypeGroup(extensions: allowedExtensions)
     ]);
@@ -38,7 +44,13 @@ class WebIO implements ArDriveIO {
   }
 
   @override
-  Future<List<IOFile>> pickFiles({List<String>? allowedExtensions}) async {
+  Future<List<IOFile>> pickFiles(
+      {List<String>? allowedExtensions,
+      FileSource fileSource = FileSource.fileSystem}) async {
+    if (fileSource != FileSource.fileSystem) {
+      throw UnsupportedPlatformException();
+    }
+
     final xFiles = await file_selector.openFiles(acceptedTypeGroups: [
       file_selector.XTypeGroup(extensions: allowedExtensions)
     ]);
