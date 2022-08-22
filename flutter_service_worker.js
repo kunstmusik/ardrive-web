e76aa5eb1e3189ab529e72120658a85f752e8d4f
matibat <matibat@gmail.com>
2022-08-22 22:16:05 +0000

    Deploying to gh-pages from @ ardriveapp/ardrive-web@96c2a93b293fb2a85e75658833f25a9d8316bb7c 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {
 "index.html": "293ba1fe4aaf594c2f13ad183e16e1ac",
 "/": "293ba1fe4aaf594c2f13ad183e16e1ac",
 "favicon.png": "9a13599a830ef123989fad7c28d09cc0",
-"version.json": "ce505f6510bb72ce74d2ec5f459f496a",
+"version.json": "ac04731d0220482ce3065fe02511111f",
 "sqlite3.wasm": "fbf9815a14460df0b4ee8b746ae9b95b",
 "manifest.json": "a610c0950a2012e01119d0e8a57e3585",
 "icons/Icon-192.png": "59fd8ea25636b975179f03f82cc7b19e",
