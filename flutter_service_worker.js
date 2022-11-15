027cde44641f7ab8ca25be8d917657d85d693b26
thiagocarvalhodev <32248947+thiagocarvalhodev@users.noreply.github.com>
2022-11-15 13:08:28 +0000

    Deploying to gh-pages from @ ardriveapp/ardrive-web@4dcf63fd49f2f95d2aeeef20950ff3423c4fb8ba 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,7 +15,7 @@ const RESOURCES = {
 "/": "293ba1fe4aaf594c2f13ad183e16e1ac",
 "manifest.json": "a610c0950a2012e01119d0e8a57e3585",
 "sqlite3.wasm": "fbf9815a14460df0b4ee8b746ae9b95b",
-"main.dart.js": "fb0e115ef0c1bd239d63403da8578b5d",
+"main.dart.js": "5bd12091f5d0d59d209d31994ff79b87",
 "assets/AssetManifest.json": "f5033e81ecc8a17dcde4617b104dace3",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/NOTICES": "c3312a3f1c16612bdd4020bb282aca86",
