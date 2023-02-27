910c74b733ac4ee2282fe98c643865a53ca73dbf
thiagocarvalhodev <32248947+thiagocarvalhodev@users.noreply.github.com>
2023-02-27 21:25:16 +0000

    Deploying to gh-pages from @ ardriveapp/ardrive-web@d931e173083757c0b27bea6ce16ec3757bc4635a 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {
 "js/arconnect.js": "fcf3565603d187e5050b091f013c4d45",
 "manifest.json": "a610c0950a2012e01119d0e8a57e3585",
 "sqlite3.wasm": "fbf9815a14460df0b4ee8b746ae9b95b",
-"version.json": "aa941a467457752d750717e5f4b23f67",
+"version.json": "3bbc2f1cb0e234d0869145aaca5ec3c2",
 "worker.js": "f916974921b8b3d98f9fdfc4b68e5432",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
