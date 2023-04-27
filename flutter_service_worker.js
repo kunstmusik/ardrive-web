8c9588733b37b26b03b2bf8394c9754705b7ed75
matibat <matibat@gmail.com>
2023-04-27 15:46:34 +0000

    Deploying to gh-pages from @ ardriveapp/ardrive-web@9bdd3e06eebdc142cbd9bf9884c6985c11230523 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "8fe11bfeab56be3d92bdb6256853160e",
+  "version.json": "879b65d33c235a4b7df8499ccae1a728",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/assets/fonts/OpenSans-Regular.ttf": "3ed9575dcc488c3e3a5bd66620bdf5a4",
